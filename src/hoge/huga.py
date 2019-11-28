b34d0ef56bcb51d32a74f514db37feff75bab4af
yamap55 <tetuya120@hotmail.com>
2019-11-28 15:58:27 +0900

    [add] pyspark code

--- src/hoge/huga.py
+++ src/hoge/huga.py
@@ -1,7 +1,20 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+
+spark = SparkSession.builder.getOrCreate()
+
 
 def piyo():
     print('piyo')
 
 
+def piyopiyo():
+    df = spark.createDataFrame([['a', 'aa', 1], ['b', 'bb', 2], ['c', 'cc', 3]], [
+                               'USER_ID', 'TARGET_DATE', 'num'])
+    df = df.agg(F.sum('num'))
+    df.show()
+
+
 if __name__ == '__main__':
     piyo()
+    piyopiyo()
